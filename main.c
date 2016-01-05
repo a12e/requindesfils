2bca2150ccd459fec1840427b2011906d3a07b92
Aurélien Brooke <aurelien.brooke@etu.unistra.fr>
2016-01-05 12:31:00 +0100

    Can now read capture from a capture file

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <getopt.h>
 #include "2_link/ethernet.h"
+#include "3_network/ip.h"
 
 int verbosity = 1;
 char errbuf[PCAP_ERRBUF_SIZE];
@@ -19,20 +20,26 @@ void packet_handler(u_char *user, const struct pcap_pkthdr *h, const u_char *byt
 }
 
 int main(int argc, char **argv) {
-    char *device = NULL;
+    char *device = NULL; // when capturing live
+    char *capture_file = NULL; // when reading capture file
     pcap_t *capture;
     bpf_u_int32 ip;
-    bpf_u_int32 mask;
+    bpf_u_int32 mask = PCAP_NETMASK_UNKNOWN;
     const char *filter = "";
     struct bpf_program bpf_program;
 
     int option;
-    while((option = getopt(argc, argv, "i:o:f:v:")) != -1) {
+    while((option = getopt(argc, argv, "hi:o:f:v:")) != -1) {
         switch(option) {
+            case 'h':
+                fprintf(stderr, "usage: %s [-v verbosity] [-f filter] [-i interface]|[-o capture_file]\n", argv[0]);
+                exit(EXIT_SUCCESS);
             case 'i':
                 device = optarg;
                 break;
-            case 'o': break;
+            case 'o':
+                capture_file = optarg;
+                break;
             case 'f':
                 filter = optarg;
                 break;
@@ -47,17 +54,31 @@ int main(int argc, char **argv) {
         }
     }
 
-    if(device == NULL && (device = pcap_lookupdev(errbuf)) == NULL) {
-        abort_pcap("pcap_lookupdev");
-    }
-    printf("using device %s for capture\n", device);
-
-    if(pcap_lookupnet(device, &ip, &mask, errbuf) != 0) {
-        abort_pcap("pcap_lookupnet");
+    if(device != NULL && capture_file != NULL) {
+        fprintf(stderr, "error: you must choose between live capture or capture file\n");
+        exit(EXIT_FAILURE);
     }
 
-    if((capture = pcap_open_live(device, 1500, 0, 100, errbuf)) == NULL) {
-        abort_pcap("pcap_open_live");
+    if(capture_file == NULL) {
+        if(device == NULL && (device = pcap_lookupdev(errbuf)) == NULL) {
+            abort_pcap("pcap_lookupdev");
+        }
+        printf("using device %s for capture (", device);
+
+        if(pcap_lookupnet(device, &ip, &mask, errbuf) != 0) {
+            abort_pcap("pcap_lookupnet");
+        }
+
+        print_ip_addr(ip); printf(" ^ "); print_ip_addr(mask); printf(")\n");
+
+        if((capture = pcap_open_live(device, 1500, 0, 100, errbuf)) == NULL) {
+            abort_pcap("pcap_open_live");
+        }
+    }
+    else {
+        if((capture = pcap_open_offline(capture_file, errbuf)) == NULL) {
+            abort_pcap("pcap_open_offline");
+        }
     }
 
     if(pcap_compile(capture, &bpf_program, filter, 0, mask) != 0) {
